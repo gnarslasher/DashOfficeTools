3bb6a69560934e9e3699b88a9ee65a8d7bcffe2e
Eric.Larson <eric.larson@mt.usda.gov>
2020-12-16 17:07:22 -0700

    added secret key to app.py for server

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import dash
 import dash_bootstrap_components as dbc
+import os
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.YETI], suppress_callback_exceptions=True)
 server = app.server
+server.secret_key = os.environ.get('secret_key', 'secret')
